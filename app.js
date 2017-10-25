5fbdd3af7177a2b9921e30b92aa3e757f98c4a73
Michael Neveu <mbneveu@gmail.com>
2017-10-25 14:59:14 -0700

    added input to add items and background

--- app.js
+++ app.js
@@ -1,24 +1,33 @@
 // TODO
-//WORKING!!!!!!!!!!!!!
-var groceryList = ['Organic Kale', 'Grape Soda', 'Apples', 'Ninja Throwing Stars'];
 
-
-
-
+//var groceryList = ['Organic Kale', 'Grape Soda', 'Apples', 'Ninja Throwing Stars'];
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      list: ['Organic Kale', 'Grape Soda', 'Apples', 'Ninja Throwing Stars']
+      list: ['Organic Kale', 'Grape Soda', 'Apples', 'Dice', 'Ninja Throwing Stars'],
+      value: 'null'
     };
   }
 
+  onButtonClick() {
+    // console.log('STATE FOR CLICK = ', this.state.value);
+    // console.log('STATE LIST = ', this.state.list);
+    var oldList = [...this.state.list];
+    oldList.push(this.state.value);
+    this.setState({list: oldList});
+    // this.setState({value: ''});
+  }
+
+  handleChange(e) { this.setState({value: e.target.value}); /*console.log(this.state.value);*/ }
+
   render () {
+    var back = {background: 'green', width: '500px', padding: '50px'};
     return (
-      <div>
-    <GroceryListInput />
-    <GroceryList list={groceryList}/>
+      <div style={back}>
+    <GroceryListInput onButtonClick={this.onButtonClick.bind(this)} forChange={this.handleChange.bind(this)}/>
+    <GroceryList list={this.state.list}/>
   </div>
     );
   }
@@ -55,30 +64,28 @@ class GroceryListInput extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: 'It is blank silly'
+      //value: 'here' //IS ONCHANGE AFFECTING THIS ONE?
     };
   }
 
-  onButtonClick() {
-    //console.log('CLICKED');
-    console.log(this.state.value);
-    groceryList.push(this.state.value);
-    this.setState({value: ''});
-  }
+  // onButtonClick() {
+  //   //console.log('CLICKED');
+  //   console.log(this.state.value);
+  //   groceryList.push(this.state.value);
+  //   this.setState({value: ''});
+  // }
 
-  handleChange(e) {this.setState({value: e.target.value});}
+  // handleChange(e) {this.setState({value: e.target.value});}
   
 
   render () {
     return (
       <div>
-        <input type='text' onChange={this.handleChange.bind(this)} value={this.state.value}></input>
-        <button id='addItemButton' onClick={this.onButtonClick.bind(this)}>Add This Item</button>
+        <input type='text' onChange={this.props.forChange}></input>
+        <button id='addItemButton' onClick={this.props.onButtonClick}>Add This Item</button>
       </div>
     );
   }
-
-
 }
 
 
